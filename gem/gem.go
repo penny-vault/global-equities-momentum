5d22a26e9cc4278d145f44a3f0b1fdcb24589141
Jeremy Fergason <jeremy@fergason.me>
2026-07-14 23:36:46 -0700

    chore: release v0.2.6

--- gem/gem.go
+++ gem/gem.go
@@ -52,8 +52,8 @@ func (s *GlobalEquitiesMomentum) Describe() engine.StrategyDescription {
 		ShortCode:   "gem",
 		Description: description,
 		Source:      "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2042750",
-		Version:     "1.0.0",
-		VersionDate: time.Date(2026, 3, 15, 0, 0, 0, 0, time.UTC),
+		Version:     "1.0.1",
+		VersionDate: time.Date(2026, 7, 14, 0, 0, 0, 0, time.UTC),
 		Schedule:    "@monthend",
 		Benchmark:   "SPY",
 	}
